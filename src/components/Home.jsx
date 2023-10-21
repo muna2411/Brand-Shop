dc0291d002a4c209d1f30bd15659312be7a09bf4
muna2411 <nurnahar.muna.2411@gmail.com>
2023-10-21 22:57:43 +0600

    complete home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,7 +30,7 @@ const handleSwitch = () =>{
     const cards = useLoaderData();
     return (
         <div className="bg-white dark:bg-black" >
-            <button onClick={handleSwitch}  className="btn lg:ml-[500px] sm:ml-[500px]" >
+            <button onClick={handleSwitch}  className="btn lg:ml-[50px] sm:ml-[30px]" >
             <label className="swap swap-rotate">
   
   {/* this hidden checkbox controls the state */}
